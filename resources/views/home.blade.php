230fc306a2fb9d94f37802dc9d35eb12f8c0dc5a
Albert Piikov <alpiik@taltech.ee>
2026-06-25 01:31:03 +0300

    title rename

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posts</title>
+    <title>Home</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
